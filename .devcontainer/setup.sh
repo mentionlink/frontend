a44897b37b05c15fe00ab3f79d5f33a64f1146ed
Paul B. <paul@mentionads.com>
2025-02-07 19:49:44 -0800

    setup both packages

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+# Copyright 2025 Mention Ads. All rights reserved.
+# Use of this source code is governed by the PolyForm Shield 1.0.0 license
+# that can be found in the LICENSE.md file at the root of this repository.
+
+pushd packages/pages
+npm ci
+npm run build
+popd
+
+pushd packages/script
+npm ci
+npm run build
+popd
