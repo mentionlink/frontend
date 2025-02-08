cb1a5c1ce0b476b32203ba81da533a8be7c4737f
Paul B. <paul@mentionads.com>
2025-02-07 19:56:31 -0800

    must use bash for pushd

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2025 Mention Ads. All rights reserved.
 # Use of this source code is governed by the PolyForm Shield 1.0.0 license
 # that can be found in the LICENSE.md file at the root of this repository.
