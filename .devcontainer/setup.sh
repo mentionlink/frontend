cee73f0dbf78e442c1eb5074c62260229031f7c0
Paul B. <info@jobsort.com>
2025-05-24 18:57:25 +0000

    Add git configuration to disable pull rebase

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,7 @@
 # Use of this source code is governed by the PolyForm Shield 1.0.0 license
 # that can be found in the LICENSE.md file at the root of this repository.
 
+git config pull.rebase false
 git config core.hooksPath .githooks
 
 pushd packages/pages
